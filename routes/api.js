8e42206f9917a34f9ff9cffa1b8bdff98bfe88ef
Todd Alloway <kinome79@yahoo.com>
2022-05-03 16:21:28 -0400

    Added comments to route

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,11 @@ module.exports = function (app) {
   
   const translator = new Translator();
 
+  //Handle Post Requests to /api/translate
   app.route('/api/translate')
     .post((req, res) => {
+
+      //Run translators translate method to translate the text and return result
       translator.translate (req.body, (err, data) => {
         if (err) {return res.json(err)};
         return res.json(data);

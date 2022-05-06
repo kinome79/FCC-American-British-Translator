586b6a7dd9b076dd6f006dceefd69dbb754eef16
Todd Alloway <kinome79@yahoo.com>
2022-05-05 20:38:54 -0400

    Typo correction and commenting

--- components/translator.js
+++ components/translator.js
@@ -5,12 +5,12 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
-    //---Method to implement a translation request----------------------
+    //---Method to perform a translation request----------------------
     translate (data, done) {
 
         //Check that all required fields provided
         if (!('text' in data) || !('locale' in data)) {
-            return done({error: "Required fields(s) missing"});
+            return done({error: "Required field(s) missing"});
         }
 
         //Expand provided variables
@@ -21,10 +21,11 @@ class Translator {
             return done ({error: "No text to translate"});
         }
 
+        //create copy of the text to modify, and boolean of translate method
         let AtoB;
         let translated = text;
 
-        //Check the local and translate appropriately
+        //Check the locale and translate appropriately
         if (locale == "american-to-british") {
             AtoB = true;
         } else if (locale == "british-to-american"){
@@ -33,7 +34,7 @@ class Translator {
             return done ({error: "Invalid value for locale field"});
         }
 
-        //Translate Time Items
+        //Build Regex for finding time items, and appropriate replacement text
         let timeRegex;
         let timeReplace;
         if (AtoB) {
@@ -44,6 +45,7 @@ class Translator {
             timeReplace = '<span class="highlight">$1:$2</span>';
         }
 
+        //Perform translation on all time items
         translated = translated.replace(timeRegex, timeReplace);
 
         //Translate Honorifics
@@ -62,6 +64,7 @@ class Translator {
         //Translate Spellings
         translated = this.performReplace(translated, americanToBritishSpelling, AtoB);
 
+        //If no change in text, return looks good, else return translated string
         if (translated == text) {
             return done (null, {text: text, translation: "Everything looks good to me!"});
         } else {
@@ -70,17 +73,25 @@ class Translator {
 
     }
 
+    //Function -takes a string, an object list of tranlation items, and direction of translation, returns translated string
     performReplace(theString, theValues, direct) {
 
+        //Go through each item in the list of translation items, replace those items if found in string
         Object.keys(theValues).forEach( (key) => {
+
+            //Set regex string and replacement string based on direction of translation
             let regexString = direct? key: theValues[key];
             const replaceString = direct? theValues[key]: key;
 
+            //If regex translation item ends in a period, mark it with \ for the regex
             if (regexString[regexString.length-1] == ".") {
                 regexString = regexString.slice(0, regexString.length-1) + "\\.";
             }
 
+            //Build new RegEx using regex string, adding end of word exclusion criteria at each end
             const theRegEx = new RegExp("(?<!\\w|-)"+regexString+"(?!\\w|-)","gi");
+
+            //Build replacement function with replacement string, and method to match case on first letter
             const theReplace = (match) => {
                 if (match[0] == match[0].toLowerCase()) {
                     return '<span class="highlight">' + replaceString + '</span>';
@@ -89,9 +100,11 @@ class Translator {
                 }
             };
 
+            //Use Regex and replacement function to replace any occurances in string
             theString = theString.replace(theRegEx, theReplace);
         });
 
+        //Return translated string
         return theString;
     }
 
